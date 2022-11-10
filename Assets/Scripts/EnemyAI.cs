4a41ee1b0a62e53a6455de178880fb301d9344ee
cc-cedeno <85382291+cc-cedeno@users.noreply.github.com>
2022-11-10 16:57:08 -0500

    Initial commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // each frame move or set destination of navMeshAgent to be whereever the player is
+        navMeshAgent.SetDestination(target.position);
+    }
+}
