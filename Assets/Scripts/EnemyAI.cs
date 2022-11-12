f6af4f1fbaf6de791680ecc0ca7d3a22c56cd372
cc-cedeno <85382291+cc-cedeno@users.noreply.github.com>
2022-11-11 19:30:31 -0500

    Minor updates + EnemyAI
    Added small updates to the scene such as objects and materials. Added more functionality to the previously created enemy AI script

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,19 +5,58 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform target; // target is the player
+    [SerializeField] float chaseRadius = 5f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     
-    // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // each frame move or set destination of navMeshAgent to be whereever the player is
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (isProvoked) // first checks if isProvoked is enabled, which will activate in else if - if player is within chaseRadius
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRadius) // enemy will always at least chase player if player steps within chaseRadius
+        {
+            isProvoked= true;
+        }
+    }
+
+    void EngageTarget()
+    {
+        // stopping distance (in navMeshAgent) is how far enemy will stop once it reaches player
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) // this will allow enemy to keep chasing even if player is outside of chaseRadius
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) // if enemy reaches player, it stops and attacks
+        {
+            AttackTarget();
+        }
+    }
+
+    void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position); // after each frame, move or set destination of navMeshAgent to be whereever the player is
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("You are under attack!");
+    }
+
+    void OnDrawGizmosSelected() // display markers for chaseRadius when enemy is selected
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
