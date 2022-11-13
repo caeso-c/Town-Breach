e5d093fcbc41b42d77282e3be3f22e06ef8d8a8d
cc-cedeno <85382291+cc-cedeno@users.noreply.github.com>
2022-11-12 20:16:47 -0500

    Particle system vfx for gun shots
    Added particle system and visual feedback of gunshots. Removed any unused assets

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera; // variable for camera
     [SerializeField] float range = 100f; // how far raycast goes
     [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect; // using game object for particle hit fx so we can spawn and destroy it
 
     void Update()
     {
@@ -18,15 +21,24 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    void ProcessRaycast()
+    {
         RaycastHit hit; // stores info of what the raycast hit
 
         // executes raycast and returns bool - did you hit something yes or no
         // position raycast shoots from, direction shot goes, what did the raycast hit, and how far did it go
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("You hit " + hit.transform.name);
-            // add hit vfx
-
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>(); // if we hit something, access enemy health component
             if (target == null) return; // avoid null when shooting an object without enemy health script attached to it
             target.TakeDamage(damage); // run take damage method in target that inflicts damage to enemy
@@ -35,6 +47,11 @@ public class Weapon : MonoBehaviour
         {
             return; // avoids null when shooting nothing
         }
-        
+    }
+
+    void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 2f);
     }
 }
